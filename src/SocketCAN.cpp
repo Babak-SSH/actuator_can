a79565b7a5f8a467b6b5f6223227ba03b772e522
Babak-SSH <babak.sefidgar@gmail.com>
2024-05-18 20:37:07 +0330

    Add: transmit functions and a thread for recieve function

--- src/SocketCAN.cpp
+++ src/SocketCAN.cpp
@@ -20,27 +20,21 @@
 
 
 SocketCAN::SocketCAN(std::function<void(can_frame_t*)> reception_handler)
-   :CANAdapter(reception_handler),
-    sockfd(-1),
-    receiver_thread_id(0)
-{
+:sockfd(-1), receiver_thread_id(0) {
     adapter_type = ADAPTER_SOCKETCAN;
     printf("SocketCAN adapter created.\n");
 }
 
 
-SocketCAN::~SocketCAN()
-{
+SocketCAN::~SocketCAN() {
     printf("Destroying SocketCAN adapter...\n");
-    if (this->is_open())
-    {
+    if (this->is_open()) {
         this->close();
     }
 }
 
 
-void SocketCAN::open(const char* interface)
-{
+void SocketCAN::open(const char* interface) {
 
     // Setting can interface
     __can_conf_1 = "sudo ip link set " + interface + " type can bitrate 1000000";
@@ -56,8 +50,7 @@ void SocketCAN::open(const char* interface)
 
     // Request a socket
     sockfd = socket(PF_CAN, SOCK_RAW, CAN_RAW);
-    if (sockfd == -1)
-    {
+    if (sockfd == -1) {
         printf("Error: Unable to create a CAN socket\n");
         return;
     }
@@ -65,8 +58,7 @@ void SocketCAN::open(const char* interface)
 
     // Get the index of the network interface
     strncpy(if_request.ifr_name, interface, IFNAMSIZ);
-    if (ioctl(sockfd, SIOCGIFINDEX, &if_request) == -1)
-    {
+    if (ioctl(sockfd, SIOCGIFINDEX, &if_request) == -1) {
         printf("Unable to select CAN interface %s: I/O control error\n", interface);
 
         // Invalidate unusable socket
@@ -83,8 +75,7 @@ void SocketCAN::open(const char* interface)
         reinterpret_cast<struct sockaddr*>(&addr),
         sizeof(addr)
     );
-    if (rc == -1)
-    {
+    if (rc == -1) {
         printf("Failed to bind socket to network interface\n");
 
         // Invalidate unusable socket
@@ -97,8 +88,7 @@ void SocketCAN::open(const char* interface)
     start_receiver_thread();
 }
 
-void SocketCAN::close()
-{
+void SocketCAN::close() {
     terminate_receiver_thread = true;
 
     if (!is_open())
@@ -111,7 +101,93 @@ void SocketCAN::close()
     printf("CAN socket destroyed.\n");
 }
 
-bool SocketCAN::is_open()
-{
+bool SocketCAN::is_open() {
     return (sockfd != -1);
 }
+
+void SocketCAN::transmit(can_frame_t* can_frame) {
+    if (!is_open()) {
+        printf("Unable to transmit: Socket not open\n");
+        return;
+    }
+    
+    int nbytes = write(this->sockfd, can_frame, sizeof(can_frame_t));
+
+    if (nbytes != sizeof(*can_frame)) {
+        printf("Transmit Error!\r\n");
+    }
+}
+
+
+static void* socketcan_receiver_thread(void* argv) {
+    /*
+     * The first and only argument to this function
+     * is the pointer to the object, which started the thread.
+     */
+    SocketCAN* sock = (SocketCAN*) argv;
+
+    // Holds the set of descriptors, that 'select' shall monitor
+    fd_set descriptors;
+
+    // Highest file descriptor in set
+    int maxfd = sock->sockfd;
+
+    // How long 'select' shall wait before returning with timeout
+    struct timeval timeout;
+
+    // Buffer to store incoming frame
+    can_frame_t rx_frame;
+
+    // Run until termination signal received
+    while (!sock->terminate_receiver_thread) {
+        // Clear descriptor set
+        FD_ZERO(&descriptors);
+        // Add socket descriptor
+        FD_SET(sock->sockfd, &descriptors);
+       printf("Added %d to monitored descriptors.\n", sock->sockfd);
+
+        // Set timeout
+        timeout.tv_sec  = 0;
+        timeout.tv_usec = 100;
+
+        // Wait until timeout or activity on any descriptor
+        if (select(maxfd+1, &descriptors, NULL, NULL, &timeout) == 1) {
+            int len = read(sock->sockfd, &rx_frame, CAN_MTU);
+        //    printf("Received %d bytes: Frame from 0x%0X, DLC=%d\n", len, rx_frame.can_id, rx_frame.can_dlc);
+
+            if (len < 0)
+                continue;
+
+            // callback function
+            if (sock->reception_handler != NULL) {
+                sock->reception_handler(&rx_frame);
+            }
+        }
+        else {
+//            printf("Received nothing.\n");
+        }
+    }
+
+    printf("Receiver thread terminated.\n");
+
+    // Thread terminates
+    return NULL;
+}
+
+
+void SocketCAN::start_receiver_thread() {
+    /*
+     * Frame reception is accomplished in a separate, event-driven thread.
+     *
+     * See also: https://www.thegeekstuff.com/2012/04/create-threads-in-linux/
+     */
+    terminate_receiver_thread = false;
+    int rc = pthread_create(&receiver_thread_id, NULL, &socketcan_receiver_thread, this);
+    if (rc != 0) {
+        printf("Unable to start receiver thread.\n");
+        return;
+    }
+    printf("Successfully started receiver thread with ID %d.\n", (int) receiver_thread_id);
+}
+
+#endif
